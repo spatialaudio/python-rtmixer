8c5d03b17af18900fe3dd7a3931c3f2fcce39ad0
Eric Larson <larson.eric.d@gmail.com>
2024-06-21 22:02:30 -0400

    FIX: Order

--- tools/cibw_before_test_linux.sh
+++ tools/cibw_before_test_linux.sh
@@ -4,6 +4,6 @@ set -xeo pipefail
 
 if [[ "$(cat /etc/redhat-release)" == 'AlmaLinux'* ]]; then
     dnf config-manager --set-enabled powertools
-    dnf install epel-release
+    dnf install -y epel-release
 fi
 yum install -y portaudio
