9fc8084bcf140c6cf6edd6283216073da800e6ea
Eric Larson <larson.eric.d@gmail.com>
2024-06-28 10:08:59 -0400

    FIX: Another

--- tools/cibw_repair_wheel_command.sh
+++ tools/cibw_repair_wheel_command.sh
@@ -9,6 +9,8 @@ if [[ "$RUNNER_OS" == "Linux" ]]; then
     auditwheel repair -w $DEST_DIR $WHEEL
 elif [[ "$RUNNER_OS" == "macOS" ]]; then
     delocate-wheel --require-archs $DELOCATE_ARCHS -w $DEST_DIR -v $WHEEL
+else
+    cp $WHEEL $DEST_DIR
 fi
 
 if [[ "$(python -c 'import platform; print(platform.python_implementation())')" == "CPython" ]]; then
